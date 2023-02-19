0d7db0b52054ed0c9090d2f1620e382eb2f19919
Gitakkkk <stom1028@naver.com>
2023-02-19 13:46:41 +0900

    23.02.19

---
+++ LeetCode/search/1. easy/101.py
@@ -0,0 +1,15 @@
+# 101. Symmetric Tree
+
+class Solution(object):
+    def isSymmetric(self, root):
+        if not root:
+            return True
+        return self.isSame(root.left, root.right)
+    def isSame(self, leftroot, rightroot):
+        if leftroot == None and rightroot == None:
+            return True
+        if leftroot == None or rightroot == None:
+            return False
+        if leftroot.val != rightroot.val:
+            return False
+        return self.isSame(leftroot.left, rightroot.right) and self.isSame(leftroot.right, rightroot.left)
